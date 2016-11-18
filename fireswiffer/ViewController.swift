44a2aca1b8cd304ba38e614a8b39746e3cd20c4c
whg3481 <tost2life@gmail.com>
2016-11-17 23:16:53 -0600

    Initial Commit

---
+++ fireswiffer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fireswiffer
+//
+//  Created by New User on 11/17/16.
+//  Copyright © 2016 Will Garner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
